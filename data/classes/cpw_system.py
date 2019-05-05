37b04ab7fe9453ad1b8de64f20f6ac453fa21709
Hector Almanza <pizzapie7738@gmail.com>
2019-05-05 01:07:17 -0500

    Started writing SystemInfo class, created app_handler.py, allow main.py to read command line arguments.

--- data/classes/cpw_system.py
+++ data/classes/cpw_system.py
@@ -1,1 +1,12 @@
-ECHO is on.
+
+
+class SystemInfo():
+	def __init__(self):
+		self.time_flag = "Random"
+		self.start_range = None
+		self.end_range = None
+		self.fixed_range = None
+		self.webhook_url = None
+	
+	def main_init(self, url, time_flag=None, start_range=None, end_range=None, fixed_range=None):
+		pass
---
+++ data/scripts/app_handler.py
@@ -0,0 +1,9 @@
+import sys
+sys.path.insert(0, "../classes")
+
+from cpw_system import SystemInfo
+
+sysInfo = SystemInfo()
+
+def __init__(url, time_setting=None, time_start=None, time_end=None, time_fixed=None):
+	sysInfo.main_init(# TODO: Add params to function)
