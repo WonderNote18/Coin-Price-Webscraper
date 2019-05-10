e61bf1d7861bff7d084b3158dbd44452b2e22d5f
Hector Almanza <pizzapie7738@gmail.com>
2019-05-09 22:14:12 -0500

    Updated cpw_log.py with AppLogger class functions and __init__

--- data/classes/cpw_log.py
+++ data/classes/cpw_log.py
@@ -1,1 +1,39 @@
-ECHO is on.
+import logging
+import datetime
+import os
+
+
+class AppLogger():
+	def __init__(self):
+		self.logger = logging.getLogger("AppLogger")
+		self.logger.setLevel(logging.DEBUG)
+
+		self.formatter = logging.Formatter("%(asctime)s[%(levelname)s]: %(message)s", "[%I:%M:%S %p]")
+
+		self.consoleHandler = logging.StreamHandler()
+		self.consoleHandler.setLevel(logging.DEBUG)
+
+		self.fileDate = datetime.datetime.now().strftime("%m-%d-%Y")
+		if (self.fileDate + ".log") not in os.listdir("../logs"):
+			newFile = open("../logs/" + self.fileDate + ".log", 'w')
+			newFile.close()
+		self.fileHandler = logging.FileHandler("../logs/" + self.fileDate + ".log", 'a')
+		self.fileHandler.setLevel(logging.DEBUG)
+		self.fileHandler.setFormatter(self.formatter)
+
+		self.logger.addHandler(self.fileHandler)
+	
+	def updateLoggingFileDaily(self, newTime):
+		self.fileDate = newTime.strftime("%m-%d-%Y")
+
+		self.fileHandler.close()
+		self.logger.removeHandler(self.fileHandler)
+
+		newFile = open("../logs/" + self.fileDate + ".log", 'w')
+		newFile.close()
+
+		self.fileHandler = logging.FileHandler("../logs/" + self.fileDate + ".log", 'a')
+		self.fileHandler.setLevel(logging.DEBUG)
+		self.fileHandler.setFormatter(self.formatter)
+
+		self.logger.addHandler(self.fileHandler)
