2c1cbfb163544c04030fe95a35396bcd9615da0d
Hector Almanza <pizzapie7738@gmail.com>
2019-05-09 22:16:17 -0500

    Edited main.py: Pull command args, __main__ function call to app_handler.__init__()

--- data/main.py
+++ data/main.py
@@ -3,15 +3,7 @@ sys.path.insert(0, '../scripts')
 
 import app_handler
 
-time_vars = ['-s', '-r']
 arg_vars = sys.argv
 
 if __name__ == "__main__":
-	if len(arg_vars) >= 2:
-		if time_vars[0] in arg_vars:
-			pass
-		elif time_vars[1] in arg_vars:
-			pass
-	else:
-		exit("Useage: main.py url [-s minutes | -r start end]")
-		
+	app_handler.__init__(arg_vars)
